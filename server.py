66147a7333c80ed21108a838918b015ef303fec5
sungjunyoung <wnsdud1861@gmail.com>
2017-10-14 04:21:14 +0900

    이미지 crop / trim -> 64 * 64
    —
    Redmine Issue Progress / ID / Time :  100% #63 @30m
    —
    What :
    - 사용자에게 입력받은 이미지를 crop
    - 사용자에게 입력받은 이미지를 64*64 로 리사이즈 (비율은 그대로/최대비로 리사이징)
    
    Why :
    - 내일 데모 ..
    —
    *Signed-off-by: Junyoung, Sung <wnsdud1861@gmail.com>*

--- server.py
+++ server.py
@@ -24,6 +24,25 @@ def testDumpResult(handwritesDic):
     return afterTestResultDic
 
 
+def scale(image, max_size, method=Image.ANTIALIAS):
+    """
+    resize 'image' to 'max_size' keeping the aspect ratio
+    and place it in center of white 'max_size' image
+    """
+    im_aspect = float(image.size[0]) / float(image.size[1])
+    out_aspect = float(max_size[0]) / float(max_size[1])
+    if im_aspect >= out_aspect:
+        scaled = image.resize((max_size[0], int((float(max_size[0]) / im_aspect) + 0.5)), method)
+    else:
+        scaled = image.resize((int((float(max_size[1]) * im_aspect) + 0.5), max_size[1]), method)
+
+    offset = (int((max_size[0] - scaled.size[0]) / 2), int((max_size[1] - scaled.size[1]) / 2))
+    print(offset)
+    back = Image.new("RGB", max_size, "white")
+    back.paste(scaled, offset)
+    return back
+
+
 # trim from base64 image
 def trimAndSave(b64Image, email, fileName):
     bytesIO = BytesIO(base64.b64decode(b64Image))
@@ -39,6 +58,8 @@ def trimAndSave(b64Image, email, fileName):
         dir = './data/{}'.format(email)
         if not os.path.exists(dir):
             os.makedirs(dir)
+
+        im = scale(im, [64, 64])
         im.save(dir + '/{}.jpg'.format(fileName), 'JPEG')
         return dir + '/{}.jpg'.format(fileName)
 
