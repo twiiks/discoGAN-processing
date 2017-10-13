63477f6b38995e26042788ecf6fa07e61a19565a
sungjunyoung <wnsdud1861@gmail.com>
2017-10-13 13:45:08 +0900

    tornado 삭제 / gunicorn 추가
    —
    Redmine Issue Progress / ID / Time :  80% #54 @30m
    —
    What :
    - tornado to gunicorn
    
    Why :
    - gunicorn 으로 HTTP 서버 대체
    —
    *Signed-off-by: Junyoung, Sung <wnsdud1861@gmail.com>*

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
+
 
 @app.route('/fontto/processing', methods=['POST'])
 def processing_fontto():
@@ -16,7 +14,6 @@ def processing_fontto():
 def test_root():
     return 'OK'
 
+
 if __name__ == '__main__':
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5959)
-    IOLoop.instance().start()
+    app.run(port=5959)
