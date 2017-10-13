a9cd43276222e4150f75f150c17df52eebedac4a
sungjunyoung <wnsdud1861@gmail.com>
2017-10-13 12:30:15 +0900

    기본적인 프로젝트 구성 + 테스트용 commit
    —
    Redmine Issue Progress / ID / Time :  50% #54 @1h
    —
    What :
    - 기본적인 프로젝트를 구성합니다.
    - post 로 이벤트를 받을 수 있는 API 를 구성해봅니다.
    
    Why :
    - flask API 작성
    —
    *Signed-off-by: Junyoung, Sung <wnsdud1861@gmail.com>*

---
+++ processing.py
@@ -0,0 +1,17 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/fontto/processing', methods=['POST'])
+def processing_fontto():
+    print(request.json)
+    return json.dumps(request.json)
+
+if __name__ == '__main__':
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5959)
+    IOLoop.instance().start()
